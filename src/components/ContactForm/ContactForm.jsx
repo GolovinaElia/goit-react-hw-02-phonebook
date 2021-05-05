b46d7d7e19a4f986b8c4994b7aad28b108722f32
GolovinaElia <eliagolovina@ukr.net>
2021-05-05 23:33:05 +0300

    Добавляет файлы

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import style from "./ContactForm.module.css";
+import style from './ContactForm.module.css';
 
-class Form extends Component {
+class ContactForm extends Component {
   state = {
     name: '',
-    number: ''
+    number: '',
   };
 
   handleChange = event => {
     const { name, value } = event.currentTarget;
-    this.setState({[name]: value});
+    this.setState({ [name]: value });
   };
 
   handleSubmit = event => {
@@ -18,45 +18,47 @@ class Form extends Component {
     this.reset();
   };
 
-    reset = () => {
-  this.setState({name: '', number: ''});
-};
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
 
   render() {
     return (
-    <div className={style.container}>
-      <form className={style.containerForm} onSubmit={this.handleSubmit}>
-        <label className={style.containerLabel}>
-          Name
-        <input
-            className={style.input}
-                type="text"
-                autoComplete="off"
-                name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            value={this.state.name}
-                onChange={this.handleChange}
-          />
-            </label>
-            <label className={style.containerLabel}>
-              Number
-               <input
-            className={style.input}
-                type="tel"
-                autoComplete="off"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-  title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            value={this.state.number}
-            onChange={this.handleChange}
-          />
-            </label>
-        <button className={style.button} type="submit">Add contact</button>
-      </form>
-    </div>
+      <div className={style.container}>
+        <form className={style.containerForm} onSubmit={this.handleSubmit}>
+          <label className={style.containerLabel}>
+            Name
+            <input
+              className={style.input}
+              type="text"
+              autoComplete="off"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label className={style.containerLabel}>
+            Number
+            <input
+              className={style.input}
+              type="tel"
+              autoComplete="off"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+              value={this.state.number}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button className={style.button} type="submit">
+            Add contact
+          </button>
+        </form>
+      </div>
     );
   }
-};
+}
 
-export default Form;
+export default ContactForm;
